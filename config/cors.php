91cf83f63707ac84e0dd3d2d6ccb2db99159c146
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2023-12-31 10:00:40 +0630

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,14 +19,16 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://sales-admin.thanywhere.com',
-        'http://staging-admin.thanywhere.com',
-        'https://staging-admin.thanywhere.com',
-        'http://mm.thanywhere.com',
-        'http://localhost:5173',
-        'http://localhost:5174'
-    ],
+    // 'allowed_origins' => [
+    //     'https://sales-admin.thanywhere.com',
+    //     'http://staging-admin.thanywhere.com',
+    //     'https://staging-admin.thanywhere.com',
+    //     'http://mm.thanywhere.com',
+    //     'http://localhost:5173',
+    //     'http://localhost:5174'
+    // ],
+
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patCterns' => [],
 
