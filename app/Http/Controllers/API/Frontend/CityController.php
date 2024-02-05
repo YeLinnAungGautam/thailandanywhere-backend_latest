6884cefbeb9295d597dda0e81301cc3051969f71
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2024-02-05 12:10:06 +0630

    Update CityController.php

--- app/Http/Controllers/API/Frontend/CityController.php
+++ app/Http/Controllers/API/Frontend/CityController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\API\Frontend;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CityResource;
 use App\Models\City;
+use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $cities = City::paginate(request('limit') ?? 10);
+        $cities = City::when($request->search, function ($query) use ($request) {
+            $query->where('name', 'LIKE', "%{$request->search}%");
+        })
+            ->paginate(request('limit') ?? 10);
 
         return CityResource::collection($cities)->additional(['result' => 1, 'message' => 'success']);
     }
