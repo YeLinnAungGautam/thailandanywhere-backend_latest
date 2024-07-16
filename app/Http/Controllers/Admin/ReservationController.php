ddf9fbadb18c2c8544eea54bec65d54060353d5e
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2024-07-16 19:04:36 +0630

    Update ReservationController.php

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -71,7 +71,7 @@ class ReservationController extends Controller
                 $q->whereIn('booking_id', function ($q) use ($request) {
                     $q->select('id')
                         ->from('bookings')
-                        ->where('created_at', $request->booking_date);
+                        ->whereDate('created_at', $request->booking_date);
                 });
             });
 
