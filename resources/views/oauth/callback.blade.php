78b3ba08e082491a0852ca6f94c425052e087db6
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2024-08-23 00:01:20 +0630

    Update callback.blade.php

--- resources/views/oauth/callback.blade.php
+++ resources/views/oauth/callback.blade.php
@@ -1,12 +1,30 @@
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <title>{{ config('app.name') }}</title>
-  <script>
-    window.opener.postMessage({ token: "{{ $token }}" }, "{{ url('/') }}")
-    window.close()
-  </script>
+    <meta charset="utf-8">
+    <title>{{ config('app.name') }}</title>
+
+    <script>
+        // window.opener.postMessage({
+        //     token: "{{ $token }}"
+        // }, "{{ url('/') }}")
+        // window.close()
+
+        window.opener.postMessage({
+            token: "{{ $token }}"
+        }, "*");
+        window.close()
+
+        // setTimeout(() => {
+        //     window.opener.postMessage({
+        //         token: "{{ $token }}"
+        //     }, "{{ url('/') }}");
+        //     window.close();
+        // }, 1000); // 1 second delay
+    </script>
 </head>
+
 <body>
 </body>
+
 </html>
