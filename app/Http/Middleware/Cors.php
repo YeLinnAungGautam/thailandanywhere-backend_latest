582735987d162d83522b1a03041b0640b7901a4d
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2024-10-13 23:35:12 +0630

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,37 +18,8 @@ class Cors
         $response = $next($request);
         $response->headers->set('Access-Control-Allow-Origin', $request->header('Origin'));
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET');
-        $response->headers->set('Access-Control-Allow-Credentials', true);
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application', 'ip');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Origin, X-Requested-With, Application', 'ip');
 
         return $response;
     }
-
-    // public function handle($request, Closure $next)
-    // {
-    //     $allowedOrigins = [
-    //         'https://sales-admin.thanywhere.com',
-    //         'https://blog.thanywhere.com',
-    //         // Add other allowed domains here
-    //     ];
-
-    //     $origin = $request->header('Origin');
-
-    //     if (in_array($origin, $allowedOrigins)) {
-    //         $headers = [
-    //             'Access-Control-Allow-Origin' => $origin,
-    //             'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-    //             'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
-    //         ];
-
-    //         $response = $next($request);
-    //         foreach ($headers as $key => $value) {
-    //             $response->header($key, $value);
-    //         }
-
-    //         return $response;
-    //     }
-
-    //     return $next($request);
-    // }
 }
