de6bb82624d7213499eb4843457344874c8a5fcc
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2024-10-13 13:35:28 +0630

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,6 @@ return [
 
     'allowed_origins' => [
         'https://sales-admin.thanywhere.com',
-        'http://sales-admin.thanywhere.com',
         'http://staging-admin.thanywhere.com',
         'https://staging-admin.thanywhere.com',
         'https://mm.thanywhere.com',
