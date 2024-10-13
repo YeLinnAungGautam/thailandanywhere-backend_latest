f3bc14b031985cf94e4bc5df9476e3a1c6bde9a8
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2024-10-14 00:00:31 +0630

    .

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,13 +13,40 @@ class Cors
      *
      * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      */
-    public function handle(Request $request, Closure $next): Response
-    {
-        $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', $request->header('Origin'));
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Origin, X-Requested-With, Application', 'ip');
+    // public function handle(Request $request, Closure $next): Response
+    // {
+    //     $response = $next($request);
+    //     $response->headers->set('Access-Control-Allow-Origin', $request->header('Origin'));
+    //     $response->headers->set('Access-Control-Allow-Methods', 'POST, GET');
+    //     $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Origin, X-Requested-With, Application', 'ip');
 
-        return $response;
+    //     return $response;
+    // }
+
+    public function handle($request, Closure $next)
+    {
+        // Define your allowed origins
+        $allowedOrigins = [
+            'https://sales-admin.thanywhere.com',
+            'http://staging-admin.thanywhere.com',
+            'https://staging-admin.thanywhere.com',
+            'https://mm.thanywhere.com',
+            'https://thanywhere.com',
+            'http://localhost:5173',
+            'http://localhost:5174'
+        ];
+
+        // Get the origin of the incoming request
+        $origin = $request->headers->get('Origin');
+
+        // Check if the origin is in the allowed list
+        if (in_array($origin, $allowedOrigins)) {
+            // Set CORS headers dynamically for allowed origins
+            header('Access-Control-Allow-Origin: ' . $origin);
+            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+            header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        }
+
+        return $next($request);
     }
 }
