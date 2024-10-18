781ce9491c07f9539241e709c9f93c601b7dcf4a
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2024-10-18 13:14:28 +0630

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -44,5 +45,14 @@ class User extends Authenticatable
     public function oauthProviders()
     {
         return $this->hasMany(OAuthProvider::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->unique_key = Str::uuid();
+        });
     }
 }
