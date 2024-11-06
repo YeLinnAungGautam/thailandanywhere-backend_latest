75f06a8fcacd36c317eda7f0016c52b846da82a9
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2024-11-06 23:31:47 +0630

    Create MigrateAttractionFeedback.php

---
+++ app/Console/Commands/MigrateAttractionFeedback.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\EntranceTicket;
+use Illuminate\Console\Command;
+
+class MigrateAttractionFeedback extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:feedback';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        EntranceTicket::chunk(100, function ($entranceTickets) {
+            foreach ($entranceTickets as $entranceTicket) {
+                $meta_data = ['is_show' => 1];
+
+                $entranceTicket->update([
+                    'meta_data' => json_encode($meta_data),
+                ]);
+            }
+        });
+    }
+}
