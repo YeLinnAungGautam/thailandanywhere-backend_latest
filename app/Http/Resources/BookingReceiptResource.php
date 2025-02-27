f42f50d4963000b4bc59ff6cca1958308bb558a5
Kaung Kaung <kaungkaung@Kaungs-Laptop.local>
2025-02-27 17:16:36 +0700

    kaung

--- app/Http/Resources/BookingReceiptResource.php
+++ app/Http/Resources/BookingReceiptResource.php
@@ -23,7 +23,7 @@ class BookingReceiptResource extends JsonResource
             'amount' => $this->amount,
             'sender' => $this->sender,
             'bank_name' => $this->bank_name,
-            'date' => $this->date,
+            'date' => $this->date->format('d-m-Y H:i:s'),
             'is_corporate' => $this->is_corporate,
             'created_at' => $this->created_at->format('d-m-Y H:i:s'),
             'updated_at' => $this->updated_at->format('d-m-Y H:i:s'),
