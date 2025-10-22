3bf0c33050911342210be474c3ca128795847fe9
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2025-10-22 16:20:21 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 
 Route::get('test', function () {
     Mail::raw('This is a test email.', function ($message) {
-        $message->to('taryarlin0088@gmail.com')
+        $message->to('dev.taryarlin@gmail.com')
             ->subject('Test Email');
     });
 
