dec49712a71ebec9d935b3b174bec22d4caf600b
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2025-12-04 19:22:00 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,9 @@ Route::get('/', function () {
 
 Route::get('test', function () {
     Mail::raw('This is a test email.', function ($message) {
-        $message->to('davidgautam.1234@gmail.com')
-            ->cc(['gg.haak0007@gmail.com'])
+        $message->to('kumarmyanmars@gmail.com')
             ->bcc(['taryarlin0088@gmail.com'])
-            ->replyTo(['dev.taryarlin@gmail.com', 'homnathgautam23@gmail.com'])
+            ->replyTo(['dev.taryarlin@gmail.com', 'ceo@thanywhere.com', 'davidgautam.1234@gmail.com'])
             ->subject('Test Email');
     });
 
