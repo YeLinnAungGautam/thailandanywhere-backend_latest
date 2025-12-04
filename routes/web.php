cf15bb37d9494aa1e1a1e0accd797be77069236e
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2025-12-04 15:33:38 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,11 @@ Route::get('/', function () {
     abort(503);
 });
 
-// Route::get('test', function () {
-//     Mail::raw('This is a test email.', function ($message) {
-//         $message->to('taryarlin0088@gmail.com')
-//             ->subject('Test Email');
-//     });
+Route::get('test', function () {
+    Mail::raw('This is a test email.', function ($message) {
+        $message->to('taryarlin0088@gmail.com')
+            ->subject('Test Email');
+    });
 
-//     return 'success';
-// });
+    return 'success';
+});
